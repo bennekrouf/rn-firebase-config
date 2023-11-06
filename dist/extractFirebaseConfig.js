d493f7ec90c9a2c35418678c3cc0cc553d369038
Mohamed <mohamed.bennekrouf@gmail.com>
2023-11-06 17:40:01 +0100

    Add package for android native module

--- dist/extractFirebaseConfig.js
+++ dist/extractFirebaseConfig.js
@@ -14,16 +14,23 @@ const react_native_1 = require("react-native");
 const mayo_logger_1 = require("mayo-logger");
 const { FirebaseConfigExtractor } = react_native_1.NativeModules;
 const extractFirebaseConfig = () => __awaiter(void 0, void 0, void 0, function* () {
-    mayo_logger_1.Logger.info('TOTO Starting Firebase config extraction...', null, { tag: 'mayo-firebase-config-extractor' });
-    const config = yield FirebaseConfigExtractor.extractConfig();
-    if (config && typeof config === 'object' && Object.keys(config).length > 0) {
-        mayo_logger_1.Logger.info('Successfully extracted Firebase config', null, { tag: 'mayo-firebase-config-extractor' });
+    try {
+        mayo_logger_1.Logger.info('Starting Firebase config extraction...', null, { tag: 'mayo-firebase-config-extractor' });
+        const config = yield FirebaseConfigExtractor.extractConfig();
+        if (config && typeof config === 'object' && Object.keys(config).length > 0) {
+            mayo_logger_1.Logger.info('Successfully extracted Firebase config', null, { tag: 'mayo-firebase-config-extractor' });
+        }
+        else {
+            mayo_logger_1.Logger.warn('Failed to extract valid Firebase config or the config is empty', null, { tag: 'mayo-firebase-config-extractor' });
+        }
+        // Be cautious about logging potentially sensitive information
+        mayo_logger_1.Logger.error('Extracted Firebase config:', { config }, { tag: 'mayo-firebase-config-extractor' });
+        return config;
     }
-    else {
-        mayo_logger_1.Logger.warn('Failed to extract valid Firebase config or the config is empty', null, { tag: 'mayo-firebase-config-extractor' });
+    catch (error) {
+        // Handle the error appropriately
+        mayo_logger_1.Logger.error('Error extracting Firebase config', error, { tag: 'mayo-firebase-config-extractor' });
+        throw error;
     }
-    // This log may expose sensitive information, so be cautious about using it in a production environment
-    mayo_logger_1.Logger.info('Extracted Firebase config:', { config }, { tag: 'mayo-firebase-config-extractor' });
-    return config;
 });
 exports.extractFirebaseConfig = extractFirebaseConfig;
--- src/extractFirebaseConfig.ts
+++ src/extractFirebaseConfig.ts
@@ -5,18 +5,24 @@ import { FirebaseConfig } from 'mayo-firebase-config';
 const { FirebaseConfigExtractor } = NativeModules;
 
 export const extractFirebaseConfig = async (): Promise<FirebaseConfig> => {
-  Logger.info('TOTO Starting Firebase config extraction...', null, { tag: 'mayo-firebase-config-extractor' });
+  try {
+    Logger.info('Starting Firebase config extraction...', null, { tag: 'mayo-firebase-config-extractor' });
 
-  const config: FirebaseConfig = await FirebaseConfigExtractor.extractConfig();
+    const config: FirebaseConfig = await FirebaseConfigExtractor.extractConfig();
 
-  if (config && typeof config === 'object' && Object.keys(config).length > 0) {
-    Logger.info('Successfully extracted Firebase config', null, { tag: 'mayo-firebase-config-extractor' });
-  } else {
-    Logger.warn('Failed to extract valid Firebase config or the config is empty', null, { tag: 'mayo-firebase-config-extractor' });
+    if (config && typeof config === 'object' && Object.keys(config).length > 0) {
+      Logger.info('Successfully extracted Firebase config', null, { tag: 'mayo-firebase-config-extractor' });
+    } else {
+      Logger.warn('Failed to extract valid Firebase config or the config is empty', null, { tag: 'mayo-firebase-config-extractor' });
+    }
+
+    // Be cautious about logging potentially sensitive information
+    Logger.error('Extracted Firebase config:', { config }, { tag: 'mayo-firebase-config-extractor' });
+
+    return config;
+  } catch (error) {
+    // Handle the error appropriately
+    Logger.error('Error extracting Firebase config', error, { tag: 'mayo-firebase-config-extractor' });
+    throw error;
   }
-
-  // This log may expose sensitive information, so be cautious about using it in a production environment
-  Logger.info('Extracted Firebase config:', { config }, { tag: 'mayo-firebase-config-extractor' });
-
-  return config;
 };
